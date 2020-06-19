47335fa9e7e0baf71414dada8be08cc0d9ace891
sowndarya <skurri@blusapphire.net>
2020-06-19 16:02:04 +0530

    Initial Commit

--- counter.js
+++ counter.js
@@ -1,14 +1,17 @@
-const counter= (data)=>{
-    var words=data.split(' ');
-   var word_counts={}
-    for(var i=0;i<words.length;i++){
+const counter = (data) => {
+    data = data.replace(", ", " ").replace(". "," ").replace(/.\s*$/, "");
+    var words = data.split(' ');
+    
+    var word_counts = {}
+    for (var i = 0; i < words.length; i++) {
 
-        if(!word_counts[words[i]]){
-            word_counts[words[i]]=1;
-        }else{
+        if (!word_counts[words[i]]) {
+            word_counts[words[i]] = 1;
+        } else {
             word_counts[words[i]]++;
         }
     }
     return word_counts;
 }
-module.exports=counter;
+
+module.exports = counter
